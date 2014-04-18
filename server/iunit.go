7a647a0c7b91b239c02e714f05f3f26697f2989b
ac7 <ac7@users.noreply.github.com>
2014-04-17 20:15:20 -0500

    Split IUnit into its own file.

---
+++ server/iunit.go
@@ -0,0 +1,29 @@
+package server
+
+const (
+	TAG_BIO = 1 << iota
+	TAG_MECH
+	TAG_FLYING
+	TAG_CLOAKED
+	TAG_SHIELDED
+	TAG_RANGED
+)
+
+type IUnit interface {
+	Name() string
+	Health() int
+	MaxHealth() int
+	Tags() int
+	HasTags(tags ...int) bool
+
+	AddTags(tags ...int)
+	RemoveTags(tags ...int)
+	TakeDamage(amount int) (alive bool)
+	SetPosition(float64, float64)
+	Position() (float64, float64)
+	Distance(other IUnit) float64
+}
+
+func isBio(unit IUnit) bool {
+	return (unit.Tags()&TAG_BIO > 0)
+}
